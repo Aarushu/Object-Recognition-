271a8582c7deeb5a38b02c883e94747315310470
Aarushu <96773012+Aarushu@users.noreply.github.com>
2022-10-27 17:20:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+img = "";
+status = "";
+
+function setup()
+{
+    canvas = createCanvas(640,420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+}
+
+function preload()
+{
+    img = loadImage('dog_cat.jpg');
+}
+
+function draw()
+{
+    image(img, 0, 0, 640, 420);
+    fill("#FF0000");
+    stroke("#FF0000");
+    text("Dog", 45, 75);
+    noFill();
+    rect(30, 60, 450, 360);
+
+    fill("#FF0000");
+    text("Cat", 320, 120);
+    noFill();
+    stroke("#FF0000");
+    rect(300, 90, 270, 320);
+}
